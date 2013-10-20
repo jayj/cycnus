e2d1dcb50e29ea9adb9a726b72fdc09991be035b
Jesper Johansen <jesper@gundlund.com>
2013-10-20 21:37:18 +0200

    Code cleanup

--- functions.php
+++ functions.php
@@ -4,13 +4,16 @@
  *
  * @package    Cycnus
  * @subpackage Functions
+ * @version    1.0.0
+ * @author     Jesper Johansen <kontakt@jayj.dk>
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 /* Add the child theme setup function to the 'after_setup_theme' hook. */
 add_action( 'after_setup_theme', 'cycnus_child_theme_setup', 11 );
 
 /**
- * Theme setup function.
+ * Theme setup function
  *
  * @since 1.0.0
  */
@@ -25,42 +28,55 @@ function cycnus_child_theme_setup() {
 	/* Add theme support for Loop Pagination. */
 	add_theme_support( 'loop-pagination' );
 
-	/* Register the Libre Baskerville font as a new heading font. */
-	add_action( 'theme_fonts_register', 'cycnus_register_font', 11 );
-
 	/* Filter the footer content. */
 	add_filter( "{$prefix}_footer_content", 'cycnus_footer_content' );
 
-	/* Filter the body class */
+	/* Filter the body class. */
 	add_filter( 'body_class', 'cycnus_body_class' );
-}
 
 function cycnus_footer_content( $content ) {
 
-	$content = '<p class="credit">' . __( 'Copyright &copy; [the-year] [site-link].<br />Powered by [wp-link], [theme-link], and [child-link].', 'cycnus' );
+	/* Register the Libre Baskerville font as a new heading font. */
+	add_action( 'theme_fonts_register', 'cycnus_register_font', 11 );
+}
 
-	return $content;
+/**
+ * Filters the default footer content to include a child theme link
+ *
+ * @param  string $content The default footer content.
+ * @return string          The filtered footer content.
+ */
+function cycnus_footer_content( $content ) {
+	return '<p class="credit">' . __( 'Copyright &copy; [the-year] [site-link].<br />Powered by [wp-link], [theme-link], and [child-link].', 'cycnus' );
 }
 
 /**
  * Extends the default WordPress body class to denote:
- *   1. White or empty background color to change the layout and spacing.
+ *   1. White or empty background color to change the layout
  *
  * @since Cycnus 1.0.0
  */
 function cycnus_body_class( $classes ) {
-        $background_color = get_background_color();
+	$background_color = get_background_color();
 
-        if ( empty( $background_color ) ) {
-                $classes[] = 'custom-background-empty';
-        } elseif ( in_array( $background_color, array( 'fff', 'ffffff' ) ) ) {
-                $classes[] = 'custom-background-white';
-        }
+	if ( empty( $background_color ) ) {
+		$classes[] = 'custom-background-empty';
+	} elseif ( in_array( $background_color, array( 'fff', 'ffffff' ) ) ) {
+		$classes[] = 'custom-background-white';
+	}
 
-        return $classes;
+	return $classes;
 }
 
 
+}
+
+/**
+ * Registers a new font for the headings
+ *
+ * @param  object $fonts
+ * @return void
+ */
 function cycnus_register_font( $fonts ) {
 
 	/* Libre Baskerville font family (normal, italic, bold). */
