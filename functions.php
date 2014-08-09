5b6a4d13de6849fd87173836903149abdd1b1b3e
Jesper Johansen <kontakt@jayj.dk>
2014-08-09 22:07:44 +0200

    New version: 1.1.1

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @package    Cycnus
  * @subpackage Functions
- * @version    1.1.0
+ * @version    1.1.1
  * @author     Jesper Johansen <kontakt@jayj.dk>
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
